a6c769f351fac9e5e90c14414cb20e551a259596
Lina Kent <linak9@nycstudents.net>
2024-10-01 10:47:02 -0400

    Added new variables + calculations + user interaction

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        String numPeople;
-        double billTotal;
-        int tipPercent;
+
+        Scanner s = new Scanner (System.in);
+        System.out.println("Note: Don't use any symbols when inputting data");
+        System.out.println("Enter your bill($):");
+        String x = s.nextLine();
+        double billTotal = Double.parseDouble(x);
+
+        System.out.println("Enter your tip percentage:");
+        String y = s.nextLine();
+        int tipPercent = Integer.parseInt(y);
+
+        System.out.println("Enter the number of people:");
+        String z = s.nextLine();
+        int numPeople = Integer.parseInt(z);
+
+
+        double tipTotal = (tipPercent * 0.01) * billTotal;
+        double billWithTip = billTotal + tipTotal;
+        double tipPerPerson = tipTotal / numPeople;
+        double billPerPerson = billWithTip / numPeople;
+
+        System.out.println("Total tip amount: $" + tipTotal);
+        System.out.println("Total bill cost: $" + billWithTip);
+        System.out.println("Tip per person: $" + tipPerPerson);
+        System.out.println("Total bill per person: $" + billPerPerson);
+
+
+
+
 
     }
 }
